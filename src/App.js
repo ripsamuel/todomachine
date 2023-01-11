cfc6af2006e8e9dd90bf813e0a9dad2eb406b135
ripsamuel <osorio.felipe@hotmail.com>
2023-01-11 12:00:34 -0500

    cambio en la descricpion de app.js

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Aprende de todo 
         </a>
       </header>
     </div>

42c7ce56bcdfb7b655e4c20bc0d5251e616ef290
ripsamuel <osorio.felipe@hotmail.com>
2023-04-12 21:04:00 -0500

    hot fix form

--- src/TodoForm/index.js
+++ src/TodoForm/index.js
@@ -31,7 +31,7 @@ function TodoForm() {
       <textarea
         value={newTodoValue}
         onChange={onChange}
-        placeholder="Cortar la cebolla oara el almuerzo"
+        placeholder="Cortar la cebolla para el almuerzo"
       />
       <div className="TodoForm-buttonContainer">
         <button
